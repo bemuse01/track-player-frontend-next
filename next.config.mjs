83ed34017e5cefaa00bccb8fcb94bc0aa29f6feb
bemuse01 <sequeland@gmail.com>
2024-11-29 23:04:08 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         remotePatterns: [
             {
                 protocol: 'https',
-                hostname: 'bemuseplayliststorage.blob.core.windows.net',
+                hostname: 'bemusetrackstorage.blob.core.windows.net',
                 port: '',
             }
         ]
